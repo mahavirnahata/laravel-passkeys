1f01126bfad1eb4edc33c0ffc0a3f4416e33e52f
Adam Marciniak <adamarc@web.de>
2025-10-28 15:42:09 +0100

    Add remember parameter to authenticateWithPasskey
    Exposes the remember functionality of AuthenticateUsingPasskeyController so it is usable with livewire / js.

--- resources/views/components/partials/authenticateScript.blade.php
+++ resources/views/components/partials/authenticateScript.blade.php
@@ -1,5 +1,5 @@
 <script>
-    async function authenticateWithPasskey() {
+    async function authenticateWithPasskey(remember = false) {
         const response = await fetch('{{ route('passkeys.authentication_options') }}')
 
         const options = await response.json();
@@ -7,8 +7,9 @@
         const startAuthenticationResponse = await startAuthentication({ optionsJSON: options, });
 
         const form = document.getElementById('passkey-login-form');
-
+    
         form.addEventListener('formdata', ({formData}) => {
+            formData.set('remember', remember);
             formData.set('start_authentication_response', JSON.stringify(startAuthenticationResponse));
         });
 
