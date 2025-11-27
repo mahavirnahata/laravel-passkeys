3dee85af5d9cf99516cf83345bf275dc59e08ac6
Pieter Naber <github.com@mantix.nl>
2025-11-27 08:59:12 +0100

    Update Dutch translations for passkeys terminology
    In Dutch, "toegangssleutel" is the correct term for Passkeys now.

--- resources/lang/nl/passkeys.php
+++ resources/lang/nl/passkeys.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'authenticate_using_passkey' => 'Authenticeren met passkey',
+    'authenticate_using_passkey' => 'Authenticeren met toegangssleutel',
     'create' => 'Aanmaken',
     'delete' => 'Verwijderen',
-    'error_something_went_wrong_generating_the_passkey' => 'Er is iets misgegaan bij het genereren van de passkey.',
-    'invalid' => 'Kon niet inloggen met de opgegeven passkey',
+    'error_something_went_wrong_generating_the_passkey' => 'Er is iets misgegaan bij het genereren van de toegangssleutel.',
+    'invalid' => 'Inloggen met de toegangssleutel is niet gelukt.',
     'last_used' => 'Laatst gebruikt',
     'name' => 'Naam',
-    'name_placeholder' => 'Voer passkey naam in',
-    'no_passkeys_registered' => 'Geen passkeys geregistreerd',
+    'name_placeholder' => 'Voer een naam voor de toegangssleutel in.',
+    'no_passkeys_registered' => 'Geen toegangssleutels geregistreerd.',
     'not_used_yet' => 'Nog niet gebruikt',
-    'passkeys' => 'Passkeys',
+    'passkeys' => 'Toegangssleutels',
 ];
